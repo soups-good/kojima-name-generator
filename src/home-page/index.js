55d7cfe02bda39e9475c365c81647dbe7edaabea
Daniel Thomas <daniel.a.thomas@avanade.com>
2020-02-05 13:32:15 -0600

    added various tylings

--- src/home-page/index.js
+++ src/home-page/index.js
@@ -12,11 +12,9 @@ class App extends Component {
     return(
       <div>
         <Header/>
-        <br></br><br></br><br></br>
         <div className="container">
           <Form/>
         </div>
-        <br></br><br></br><br></br>
         <div className = "footer">All of the ideas here are credited to <a href="https://twitter.com/briamgilbert" target="_blank" rel="noopener noreferrer">brian david gilbert</a>. Check out my inspiration <a href="https://www.youtube.com/watch?v=t-3i6GBYvdw" target="_blank" rel="noopener noreferrer">here!</a></div>
       </div>      
     )
